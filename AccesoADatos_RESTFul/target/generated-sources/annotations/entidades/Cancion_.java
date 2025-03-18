0838d6e5f7453825d43a83d8855b33801ec725aa
Victoro2304 <victor.encinas244821@potros.itson.edu.mx>
2025-03-17 19:45:07 -0700

    Se agregó el jar de la entidad en el servidor y el cliente

--- AccesoADatos_RESTFul/target/generated-sources/annotations/entidades/Cancion_.java
+++ AccesoADatos_RESTFul/target/generated-sources/annotations/entidades/Cancion_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-03-17T18:51:39", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-03-17T19:42:21", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Cancion.class)
 public class Cancion_ { 
 
